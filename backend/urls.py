61248c9648b395026be767bdae963656748527c4
cambaruk <cambaruk@my.centennialcollege.ca>
2022-02-14 20:20:28 -0500

    project setup

---
+++ backend/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+from registration import views as registration
+from django.urls import include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("auth/", include('rest_framework.urls')),
+    path("register/", registration.Register.as_view())
+]
