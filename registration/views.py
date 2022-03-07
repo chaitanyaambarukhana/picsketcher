6ca8ab4b9200be802d98caf243b07bee0521e138
chaitanyaambarukhana <chaitanyaambarukhana23@gmail.com>
2022-03-07 16:19:53 -0500

    project setup

--- registration/views.py
+++ registration/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import RegisteredUsers
 
@@ -9,6 +10,8 @@ import cv2
 import base64
 
 # Create your views here.
+
+
 class Register(APIView):
     def post(self, request):
         email = request.data["email"]
@@ -17,35 +20,25 @@ class Register(APIView):
 
         try:
             user = RegisteredUsers.objects.create(
-            email=email,
-            password=password,
-            username=username
-        )
+                email=email,
+                password=password,
+                username=username
+            )
             user.save()
         except:
-            return Response("Please enter a unique username or email")
+            return Response("Please enter a unique username or email", status=status.HTTP_200_OK)
 
-        return Response({"success":True,"message":"user successfully registered"})
+        return Response({"success": True, "message": "user successfully registered"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class Login(APIView):
     def post(self, request):
         email = request.data["email"]
         password = request.data["password"]
-        
+
         try:
             user = RegisteredUsers.objects.get(email=email)
             if user.password == password:
-                return Response({"success":True, "message":"Successfully logged in"})
+                return Response({"success": True, "message": "Successfully logged in"}, status=status.HTTP_200_OK)
         except:
-            return Response("User with the given email does not exist")
-        
-               
-               
-
-        
-    
-    
-    
-    
-    
+            return Response("User with the given email does not exist", status=status.HTTP_400_BAD_REQUEST)
