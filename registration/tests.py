215e092e1a54b77f05434d46a5c1ad674aa7039c
chaitanyaambarukhana <chaitanyaambarukhana23@gmail.com>
2022-03-07 17:06:31 -0500

    test_cases and API imporvisation. Niyanta and Chaitany a

--- registration/tests.py
+++ registration/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from rest_framework.test import RequestsClient
+from registration.views import *
+from registration.models import RegisteredUsers
+
+
+# Create your tests here.
+class SigninTest(TestCase):
+
+    def setUp(self):
+
+        self.client = RequestsClient()
+
+    def test_create_user(self):
+        view_user = Register.as_view()
+        user = {"email": "testemail@mail.com",
+                "password": "testpassword", "username": "testuser"}
+        response = self.client.post(
+            "http://localhost:8000/register/", json=user)
+        assert response.status_code == 200
+
+    def test_duplicate_user(self):
+        view_user = Register.as_view()
+        user1 = {"email": "testemail@mail.com",
+                 "password": "testpassword", "username": "testuser"}  # register user
+        user2 = {"email": "testemail1@mail.com",
+                 "password": "testpassword", "username": "testuser"}  # duplicate username
+        user3 = {"email": "testemail@mail.com",
+                 "password": "testpassword", "username": "testuser1"}  # duplicate email
+
+        response1 = self.client.post(
+            "http://localhost:8000/register/", json=user1)
+        response2 = self.client.post(
+            "http://localhost:8000/register/", json=user2)
+        response3 = self.client.post(
+            "http://localhost:8000/register/", json=user3)
+
+        assert response1.status_code == 200
+        assert response2.status_code == 400
+        assert response3.status_code == 400
--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,4 @@
+from dataclasses import field
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -14,6 +15,15 @@ import base64
 
 class Register(APIView):
     def post(self, request):
+
+        field_names = sorted(
+            [field.name for field in RegisteredUsers._meta.get_fields()])
+        request_keys = sorted(request.data.keys())
+        field_names.remove("id")
+
+        if request_keys != field_names:
+            return Response("Some fields are missing.", status=status.HTTP_400_BAD_REQUEST)
+
         email = request.data["email"]
         password = request.data["password"]
         username = request.data["username"]
@@ -26,9 +36,9 @@ class Register(APIView):
             )
             user.save()
         except:
-            return Response("Please enter a unique username or email", status=status.HTTP_200_OK)
+            return Response("Please enter a unique username or email", status=status.HTTP_400_BAD_REQUEST)
 
-        return Response({"success": True, "message": "user successfully registered"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"success": True, "message": "user successfully registered"}, status=status.HTTP_200_OK)
 
 
 class Login(APIView):
