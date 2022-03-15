9a3f5abde3fa71579888cc415aed611a919bd239
chaitanyaambarukhana <chaitanyaambarukhana23@gmail.com>
2022-03-15 12:42:02 -0400

    .

--- registration/views.py
+++ registration/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Token
+from .models import RegisteredUsers, Token
 import jwt
 import datetime
 import re
